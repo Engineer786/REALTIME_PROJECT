15731b1ee1a9333e4aa4a9b1c17cf120eda888c6
saheb <khanmdsaheb@gmail.com>
2023-07-18 13:09:41 +0530

    dictionary logic

--- python_file.py
+++ python_file.py
@@ -1,5 +1,10 @@
-def sum(a,b):
-    c=a+b
-    return c
-s=sum(10,20)
-print(s)
+def Dict(sample_dict):
+  result=''
+  for i in range(len(sample_dict['name'])):
+    name=sample_dict['name'][i]
+    numbers=sample_dict['lucky_numbers'][i]
+    for number in numbers:
+      result+=name+' '+str(number)+','
+  res1=result[:-1]
+  print(res1)
+Dict({'name':['Ganesh','Manas','Prachi'],'lucky_numbers':[[1,2,3],[2,7],[2,8,9,11]]})
